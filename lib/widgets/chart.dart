7c3b382885d60504d64f12e9df2d9c581d3ca8c3
Stanis <stanisvel.dev@gmail.com>
2022-11-14 20:30:24 +0100

    commit

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({Key? key}) : super(key: key);
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double totalSum;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+              
+            }
+      }
+
+      return {'day': DateFormat.E(weekDay), 'amount': 9.99};
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(15),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
   }
 }
